c41169956315520df3b9819e10142fa883ef2b5b
Ting Yew <62835795+ktingyew@users.noreply.github.com>
2026-07-26 23:49:37 +0800

    GWAM to GEMS
    Note that city conquest gives instant boost to science and gold

--- (2) Vox Populi/Database Changes/Text/en_US/Civilizations/CivilizationTextChanges.sql
+++ (2) Vox Populi/Database Changes/Text/en_US/Civilizations/CivilizationTextChanges.sql
@@ -248,11 +248,11 @@ WHERE Tag = 'TXT_KEY_BUILDING_STELE_STRATEGY';
 -- France
 --------------------
 UPDATE Language_en_US
-SET Text = 'La Grande Armée'
+SET Text = 'L''Académie'
 WHERE Tag = 'TXT_KEY_TRAIT_ENHANCED_CULTURE_SHORT';
 
 UPDATE Language_en_US
-SET Text = 'Melee and Gunpowder Units may enlist defeated enemies, which do not consume [ICON_WAR] Military Supply. +1 [ICON_CULTURE] Culture and [ICON_TOURISM] Tourism in every City for every 10 Military Units. +15% [ICON_SILVER_FIST] Military Supply from Population.'
+SET Text = '+10% [ICON_STRENGTH] Combat Strength for each subsequent attack against a single target during a turn. When you conquer a City, gain 50% progress toward a [ICON_GREAT_ENGINEER] Great Engineer, [ICON_GREAT_MERCHANT] Great Merchant, and [ICON_GREAT_SCIENTIST] Great Scientist in the [ICON_CAPITAL] Capital, plus a burst of [ICON_RESEARCH] Science and [ICON_GOLD] Gold scaling with the conquered City''s [ICON_CITIZEN] Population.'
 WHERE Tag = 'TXT_KEY_TRAIT_ENHANCED_CULTURE';
 
 UPDATE Language_en_US
